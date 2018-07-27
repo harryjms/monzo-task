5a3dd5283569a89894e5e4ab10fc9fb9a74208ba
Harry Jarman <me@harryjms.uk>
2018-07-27 21:42:03 +0100

    Base setup with Flow, React, Babel and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app/index.jsx',
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new FlowBabelWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: 'body',
+      template: path.join(__dirname, 'src/html/index.html')
+    })
+  ]
+};

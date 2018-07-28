024684a990e9a6eb54b93c2752f1cc28f226dad5
Harry Jarman <me@harryjms.uk>
2018-07-28 17:14:30 +0100

    Manage cookies

---
+++ src/app/Utils/CookieJar.js
@@ -0,0 +1,29 @@
+//@flow
+import moment from 'moment';
+
+const setCookie = (key: string, value: any, expires: string = ''): void => {
+    if (expires) {
+        expires = `expires=${moment(expires).format(
+            'ddd, D MMM YYYY HH:mm:ss z',
+        )};`;
+    }
+
+    document.cookie = `${key}=${value}; ${expires}path=/;`;
+};
+
+const getCookie = (key: string): any => {
+    const jar = decodeURIComponent(document.cookie);
+    const cookie = key + '=';
+    const found = jar.split(';').filter(c => c.indexOf(cookie) === 0)[0];
+    if (found) {
+        return found.substr(cookie.length, found.length);
+    } else {
+        return;
+    }
+};
+
+const deleteCookie = (key: string): void => {
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
+
+export default { setCookie, getCookie };
